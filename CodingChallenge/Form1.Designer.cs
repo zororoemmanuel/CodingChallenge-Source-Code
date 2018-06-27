abf91b921ef4acdf8be013737d8712097f23275c
zororoemmanuel <40637481+zororoemmanuel@users.noreply.github.com>
2018-06-27 17:40:45 +0200

    Initial commit

---
+++ CodingChallenge/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CodingChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtfilename = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtStringCode = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtfilename
+            // 
+            this.txtfilename.Enabled = false;
+            this.txtfilename.Location = new System.Drawing.Point(277, 13);
+            this.txtfilename.Name = "txtfilename";
+            this.txtfilename.Size = new System.Drawing.Size(219, 22);
+            this.txtfilename.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(340, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search for Equivalent";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Browse for Text File...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtStringCode
+            // 
+            this.txtStringCode.Location = new System.Drawing.Point(110, 63);
+            this.txtStringCode.Name = "txtStringCode";
+            this.txtStringCode.Size = new System.Drawing.Size(209, 22);
+            this.txtStringCode.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter String";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 445);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtStringCode);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtfilename);
+            this.Name = "Form1";
+            this.Text = "SEARCH For A MATCH";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtfilename;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtStringCode;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CodingChallenge/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CodingChallenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string m_filename = string.Empty;
+        private string m_string = string.Empty;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //open file dialog
+                OpenFileDialog ofd = new OpenFileDialog();
+                //ofd.Filter = ".txt";
+                if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    if(!string.IsNullOrEmpty(ofd.FileName))
+                    {
+                        txtfilename.Text = ofd.FileName;
+                        m_filename = ofd.FileName;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+                {
+                    throw ex;
+                }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtStringCode.Text) && !string.IsNullOrEmpty(m_filename))
+                {
+                    //read the file
+                    m_string = txtStringCode.Text.Trim();
+                    var content = System.IO.File.ReadAllText(m_filename).Split(',');
+                    Dictionary<string, string> match = new Dictionary<string, string>();
+                    //logic to match the strings
+                    foreach (string code in content)
+                    {
+                        if (code.Length != m_string.Length)
+                        {
+                            break;
+                        }
+
+                        bool matchfound = false;
+                        foreach (char chr in m_string)
+                        {
+                            if (code.ToUpper().Contains(chr.ToString().ToUpper()))
+                                matchfound = true;
+                            else
+                            {
+                                matchfound = false;
+                                break;
+                            }
+                        }
+                        if (matchfound)
+                        {
+                            match.Add(code, m_string);
+                        }
+                    }
+                    if (match.Count() > 0)
+                    {
+                        string matchfound = "";
+                        foreach (var item in match)
+                        {
+                            matchfound = matchfound + "," + item.Key;
+                        }
+                        MessageBox.Show("Congratulations The following strings are equivalent to what you entered: " + matchfound);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sorry there are no equivalent strings in this files: ");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                txtfilename.Text = "";
+                txtStringCode.Text = "";
+            }
+        }
+    }
+}
